2802af89cac2d76135cd0c05db2413b05e843815
s.sotnyk <serge.sotnyk@gmail.com>
2023-07-12 14:20:18 +0300

    layout experiments

--- field_layout.py
+++ field_layout.py
@@ -4,15 +4,16 @@ import flet as ft
 def main(page: ft.Page):
     page.add(
         ft.Row([
-            ft.Column(
-                [ft.Container(ft.Text(f"{i}"), alignment=ft.alignment.center) for i in range(3)],
-                alignment=ft.alignment.top_right),
+            ft.Column([ft.Container(ft.Text(f"{i}")) for i in range(3)],
+                      alignment=ft.MainAxisAlignment.START, height=300),
             ft.Container(ft.Text("Game field"), width=300, height=300, bgcolor=ft.colors.TEAL,
                          alignment=ft.alignment.center),
             ft.Column([ft.Container(ft.Text(f"{i}")) for i in range(3)],
-                      alignment=ft.alignment.bottom_left),
-        ])
+                      alignment=ft.MainAxisAlignment.END, height=300),
+            ],
+        )
     )
+    # page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
 
 ft.app(main, "Layout example")
