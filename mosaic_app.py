7d9282f66f55019681b727411ba9a03e60b0b103
s.sotnyk <serge.sotnyk@gmail.com>
2023-08-01 23:08:52 +0300

    files to deploy on fly.io https://flet.dev/docs/guides/python/deploying-web-app/hosting-providers/fly-io

--- mosaic_app.py
+++ mosaic_app.py
@@ -7,11 +7,17 @@ def main(page: ft.Page):
     page.title = "Mosaic game"
     page.description = "Mosaic game implemented on Python+flet"
     page.author = "S.Sotnyk"
-    page.add(game := MosaicControl())
+    page.add(
+        ft.Container(
+            game := MosaicControl(),
+            width=790,
+            height=810,
+        )
+    )
     page.window_height = 810
     page.window_width = 800
     game.init_game()
     page.update()
 
 
-ft.app(main, "Mosaic game")
+ft.app(main, "Mosaic game", view=ft.WEB_BROWSER)
